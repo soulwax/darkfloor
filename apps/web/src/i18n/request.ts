992e1264e066a45d29185bb16a454bd857deb7f6
soulwax <soulwax-95@protonmail.com>
2026-03-11 06:58:25 +0100

    feat(i18n): refactor locale retrieval to use await for cookie and header access

--- apps/web/src/i18n/request.ts
+++ apps/web/src/i18n/request.ts
@@ -3,8 +3,11 @@ import { getRequestConfig } from "next-intl/server";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  const cookieLocale = normalizeLocale(cookies().get("NEXT_LOCALE")?.value);
-  const acceptedLocale = normalizeLocale(headers().get("accept-language"));
+  const cookieStore = await cookies();
+  const requestHeaders = await headers();
+
+  const cookieLocale = normalizeLocale(cookieStore.get("NEXT_LOCALE")?.value);
+  const acceptedLocale = normalizeLocale(requestHeaders.get("accept-language"));
   const requestedLocale = normalizeLocale(await requestLocale);
 
   const locale =
